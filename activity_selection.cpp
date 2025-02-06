5737c217bbdbbb0fdd6c9767232d5660b9a4f86a
SAI SURESH GULVE <137274087+Saigulve2004@users.noreply.github.com>
2025-02-06 22:08:13 +0530

    Add files via upload

--- activity_selection.cpp
+++ activity_selection.cpp
@@ -1,58 +1,76 @@
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <bits/stdc++.h>
 #include <chrono>
-using namespace std;
 
-// A job has a start time and finish time.
+using namespace std;
+using namespace std::chrono;
+
 struct Activity {
     int start, finish;
 };
 
-// A utility function that is used for sorting
-// activities according to finish time using Selection Sort
-void selectionSort(Activity arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j].finish < arr[minIndex].finish) {
-                minIndex = j;
-            }
+// Partition function for QuickSort (sorting by finish time)
+int partition(Activity arr[], int low, int high) {
+    Activity pivot = arr[high]; // Choosing the last element as the pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j].finish < pivot.finish) {
+            i++;
+            swap(arr[i], arr[j]);
         }
-        // Swap the found minimum element with the first element
-        if (minIndex != i) {
-            swap(arr[i], arr[minIndex]);
-        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// QuickSort function
+void quickSort(Activity arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
+// Function to print maximum number of non-overlapping activities
 void printMaxActivities(Activity arr[], int n) {
-    // Sort activities according to finish time
-    selectionSort(arr, n);
+    quickSort(arr, 0, n - 1); // Sorting activities based on finish time
 
     cout << "Following activities are selected:\n";
 
-    // The first activity always gets selected
     int i = 0;
     cout << "(" << arr[i].start << ", " << arr[i].finish << ")";
 
-    // Consider rest of the activities
     for (int j = 1; j < n; j++) {
-        // If this activity has start time greater than or
         if (arr[j].start >= arr[i].finish) {
             cout << ", (" << arr[j].start << ", " << arr[j].finish << ")";
             i = j;
         }
     }
+    cout << endl;
 }
 
-// Driver code
 int main() {
-    Activity arr[] = { { 5, 9 }, { 1, 2 }, { 3, 4 },
-                       { 0, 6 }, { 5, 7 }, { 8, 9 } };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    cout << "Enter number of activities: ";
+    cin >> n;
 
-    // Function call
+    Activity arr[n];
+
+    cout << "Enter start and finish times of activities:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].start >> arr[i].finish;
+    }
+
+    auto start = high_resolution_clock::now();
+
     printMaxActivities(arr, n);
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+
+    cout << "Execution time: " << duration.count() << " microseconds\n";
+
     return 0;
 }
+
